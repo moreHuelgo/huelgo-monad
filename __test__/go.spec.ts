9f3a7170bf00687bc49fb72aca22c5b03d2f5d7b
zkfmapf123 <47292546+zkfmapf123@users.noreply.github.com>
2022-12-10 15:14:58 +0900

    [ADD] go

---
+++ __test__/go.spec.ts
@@ -0,0 +1,37 @@
+import { concat } from 'lodash'
+import { go, asyncGo } from '../src/go'
+
+describe('go test', () => {
+  it('[TEST] go', (done) => {
+    const str = go(
+      'leedonggyu',
+      (str: string) => str.replace('e', 'K'),
+      (str: string) => str.substring(0, 3)
+    )
+
+    expect(str).toBe('lKe')
+    done()
+  })
+
+  it('[TEST] async go', async () => {
+    const addP1 = (num: number) => new Promise((res, rej) => res(num + 10))
+    const minP1 = (num: number) => new Promise((res, rej) => res(num - 5))
+    const mulP1 = (num: number) => new Promise((res, rej) => res(num * 100))
+
+    const value = await asyncGo(10, addP1, minP1, mulP1)
+
+    expect(value.isOk).toBe(true)
+    expect(value.value).toBe(1500)
+  })
+
+  it('[TEST] async go fail', async () => {
+    const addP1 = (num: number) => new Promise((res, rej) => res(num + 10))
+    const minP1 = (num: number) => new Promise((res, rej) => res(num - 5))
+    const mulP1 = (num: number) => new Promise((res, rej) => rej(`${num} is too low`))
+
+    const value = await asyncGo(10, addP1, minP1, mulP1)
+
+    expect(value.isOk).toBe(false)
+    expect(value.value).toBe(`15 is too low`)
+  })
+})
