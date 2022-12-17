0c2601e9f812b6f643df5343a53405efe15d1d86
zkfmapf123 <47292546+zkfmapf123@users.noreply.github.com>
2022-12-17 13:45:38 +0900

    [ADD] mixedGo

--- __test__/go.spec.ts
+++ __test__/go.spec.ts
@@ -1,4 +1,4 @@
-import { go, asyncGo } from '../src/pipe/go'
+import { asyncGo, go, mixedGo } from '../src/pipe/go'
 
 describe('go test', () => {
   it('[TEST] go', (done) => {
@@ -32,5 +32,35 @@ describe('go test', () => {
 
     expect(value.isOk).toBe(false)
     expect(value.value).toBe(`15 is too low`)
+  })
+
+  it('[TEST] mixed go pass', async () => {
+    const mul = async (num) => num * 10
+
+    const passValue = await mixedGo(
+      10,
+      (num: number) => num + 10,
+      mul,
+      (num: number) => num + 20,
+      mul
+    )
+
+    expect(passValue.isOk).toBe(true)
+    expect(passValue.value).toBe(2200)
+  })
+
+  it('[TEST] mixed go fail', async () => {
+    const mul = async (num) => {
+      throw Error('err')
+    }
+
+    const failValue = await mixedGo(
+      10,
+      (num: number) => num + 10,
+      mul,
+      (num: number) => num + 20,
+      mul
+    )
+    expect(failValue.isOk).toBe(false)
   })
 })
--- src/pipe/go.ts
+++ src/pipe/go.ts
@@ -1,4 +1,10 @@
+//////////////////////////////////////////////// interface ////////////////////////////////////////////
+
+const isPromise = (fn) => fn?.constructor?.name === 'AsyncFunction'
+//////////////////////////////////////////////// interface ////////////////////////////////////////////
+
 /**
+ * 동기식 형태 함수형 프로그래밍
  * go
  */
 export const go = <T>(initParam: T, ...fns): T => {
@@ -7,9 +13,10 @@ export const go = <T>(initParam: T, ...fns): T => {
 }
 
 /**
+ * 비동기식 형태 함수형 프로그래밍
  * asyncGo
  */
-export const asyncGo = async <E, T>(initParams: T, ...promiseFns): Promise<{ isOk: boolean; value: T }> => {
+export const asyncGo = async <T>(initParams: T, ...promiseFns): Promise<{ isOk: boolean; value: T }> => {
   try {
     for (const promiseFn of promiseFns) initParams = await promiseFn(initParams)
     return {
@@ -23,3 +30,33 @@ export const asyncGo = async <E, T>(initParams: T, ...promiseFns): Promise<{ isO
     }
   }
 }
+
+/**
+ * 동기 + 비동시기 형태 함수형 프로그래밍
+ */
+export const mixedGo = async <T>(initParams: T, ...funcs): Promise<{ isOk: boolean; value: T }> => {
+  try {
+    for (const func of funcs) {
+      if (isPromise(func)) {
+        initParams = await func(initParams)
+        continue
+      }
+
+      initParams = func(initParams)
+    }
+
+    return {
+      isOk: true,
+      value: initParams,
+    }
+  } catch (e) {
+    return {
+      isOk: false,
+      value: e,
+    }
+  }
+}
+
+/**
+ * condition go
+ */
